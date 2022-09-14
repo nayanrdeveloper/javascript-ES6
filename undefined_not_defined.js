65779216b9fbc7605d697c0ee91b565d225e2862
nayanrdeveloper <nayanrdeveloper@gmail.com>
2022-09-14 15:18:50 +0530

    not defined and undefined

---
+++ undefined_not_defined.js
@@ -0,0 +1,8 @@
+// console.log(a);  // not defined (memory are not allocated)
+let a = 90;
+console.log(a);
+
+let b;
+console.log(b); //undefined - if any value not store so default value are undefined;
+b= 20;  // now b value change undefined to 20
+console.log(b);
